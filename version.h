e2c3dd1b55a743cd39b2e2aab3e86c3e4e1827f6
Luca_Fabbrini <luca.fabbrini@stud.unifi.it>
2022-04-24 18:13:55 +0200

    Aggiunto codice versione programma

---
+++ version.h
@@ -0,0 +1,11 @@
+//
+// Created by ardui on 24/04/2022.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "V1.0"
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
